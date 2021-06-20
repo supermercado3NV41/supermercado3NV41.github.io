012373fb7633f202db5f8368368005307cf97025
supermercado3NV41 <86180271+supermercado3NV41@users.noreply.github.com>
2021-06-20 02:06:34 -0500

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -45,7 +45,7 @@ class MiNav extends HTMLElement {
           </li>
           <li>
             <a href=
-"pasatiempos.html">Pasatiempos</a>
+"pasatiempos.html">Articulos</a>
           </li>`;
       }
       /* Enlaces para solo
