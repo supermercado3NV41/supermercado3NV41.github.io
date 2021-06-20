22abb05f4d1f6753ccc1a9f37f32614513d7b6b1
supermercado3NV41 <86180271+supermercado3NV41@users.noreply.github.com>
2021-06-20 09:30:13 -0500

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -19,58 +19,8 @@ const SIN_PASATIEMPO = /* html */
 const firestore = getFirestore();
 const daoRol = firestore.
   collection("Rol");
-const daoPasatiempo = firestore.
-  collection("Pasatiempo");
 const daoUsuario = firestore.
   collection("Usuario");
-
-/**
- * @param {
-    HTMLSelectElement} select
- * @param {string} valor */
-export function
-  selectPasatiempos(select,
-    valor) {
-  valor = valor || "";
-  daoPasatiempo.
-    orderBy("nombre").
-    onSnapshot(
-      snap => {
-        let html = SIN_PASATIEMPO;
-        snap.forEach(doc =>
-          html += htmlPasatiempo(
-            doc, valor));
-        select.innerHTML = html;
-      },
-      e => {
-        muestraError(e);
-        selectPasatiempos(
-          select, valor);
-      }
-    );
-}
-
-/**
- * @param {
-  import("../lib/tiposFire.js").
-  DocumentSnapshot} doc
- * @param {string} valor */
-function
-  htmlPasatiempo(doc, valor) {
-  const selected =
-    doc.id === valor ?
-      "selected" : "";
-  /**
-   * @type {import("./tipos.js").
-                  Pasatiempo} */
-  const data = doc.data();
-  return (/* html */
-    `<option
-        value="${cod(doc.id)}"
-        ${selected}>
-      ${cod(data.nombre)}
-    </option>`);
-}
 
 /**
  * @param {HTMLElement} elemento
@@ -147,15 +97,11 @@ export async function
     id) {
   try {
     evt.preventDefault();
-    const pasatiempoId =
-      getForánea(formData,
-        "pasatiempoId");
     const rolIds =
       formData.getAll("rolIds");
     await daoUsuario.
       doc(id).
       set({
-        pasatiempoId,
         rolIds
       });
     const avatar =
