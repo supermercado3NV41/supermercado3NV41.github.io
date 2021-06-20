352b2555198949e925c5e355375a1ebd822048e0
supermercado3NV41 <86180271+supermercado3NV41@users.noreply.github.com>
2021-06-20 06:16:49 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -4,7 +4,7 @@ class MiFooter
     this.innerHTML = /* html */
       `<p>
         &copy; 2021
-        Rojas Rodríguez Roberto Yael prueba.
+        Rojas Rodríguez Roberto Yael.
       </p>`;
   }
 }
